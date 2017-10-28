c4bbdf01d8f3e52b4483b863483c5e55fa565c3c
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2017-10-28 13:36:10 +0300

    Add the preferences model class
    This class contains the preferred number of questions, number of choices
    and regions.

---
+++ app/src/main/java/ua/edu/lnu/ami/flagsquiz/models/Preferences.java
@@ -0,0 +1,38 @@
+package ua.edu.lnu.ami.flagsquiz.models;
+
+import java.util.List;
+
+/**
+ * <p>Represents the application preferences.</p>
+ * @author Tolik Pylypchuk
+ */
+public class Preferences {
+	
+	private int numQuestions;
+	private int numChoices;
+	private List<Region> regions;
+	
+	public int getNumQuestions() {
+		return numQuestions;
+	}
+	
+	public void setNumQuestions(int numQuestions) {
+		this.numQuestions = numQuestions;
+	}
+	
+	public int getNumChoices() {
+		return numChoices;
+	}
+	
+	public void setNumChoices(int numChoices) {
+		this.numChoices = numChoices;
+	}
+	
+	public List<Region> getRegions() {
+		return regions;
+	}
+	
+	public void setRegions(List<Region> regions) {
+		this.regions = regions;
+	}
+}
